4ab1760427803255f5d7b27dcc87daec66dc262f
huomeng <455801665@qq.com>
2020-07-11 13:49:06 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import X from "./x.js";
-console.log(X);
+import png from "./assets/1.png";
+console.log(png);
+const div = document.getElementById("app");
+console.log(div);
+div.innerHTML = `
+<img src="${png}"></img>
+`;
+const button = document.createElement("button");
+
+button.innerHTML = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {
+      console.log("加载失败");
+    }
+  );
+};
+div.appendChild(button);
